e0c13beccd59951a023b0ea3054d7a27afa8ff4b
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-12-05 11:04:40 +0000

    add tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Segoe UI",Roboto,Oxygen,Ubuntu, Cantarell,"Open Sans","Helvetica Neue",sans-serif;', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        background: "#000212",
+      }
+    },
   },
   plugins: [],
 }
