a7da707de39e90a6ccf2f127469c38fcb8092a05
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-12-05 14:20:48 +0000

    add logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+import Container from "./container";
+import { Logo } from "./icons/logo";
 
 
 
 export default function Header() {
     return (
-        <>
-        Header here
-        </>
+        <header>
+            <Container>
+                <Link href="/" className="flex gap-2 items-center"><Logo className="h-[1.8rem] w-[1.8rem]" /> Linear</Link>
+            </Container>
+        </header>
     )
 }
