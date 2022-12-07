3e476fe35f5062f752cf4fbf4eebd9dfc00c31d5
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-12-07 10:10:32 +0000

    text shadow

--- components/header.tsx
+++ components/header.tsx
@@ -5,13 +5,13 @@ import Button from "./button";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 w-full border-b border-white-a08">
+    <header className="fixed top-0 left-0 w-full border-b border-white-a08 backdrop-blur-[12px]">
       <Container className="flex h-[var(--navigation-height)]">
         <Link href="/" className="flex gap-2 items-center text-md">
           <Logo className="h-[1.8rem] w-[1.8rem]" /> Linear
         </Link>
         <nav className="h-full">
-          <ul className="flex items-center gap-6 h-full [&_a]:text-sm">
+          <ul className="flex items-center gap-6 h-full [&_a]:text-sm [&_a:hover]:text-grey [&_a]:transition-colors">
             <li>
               <Link href="#">Features</Link>
             </li>
