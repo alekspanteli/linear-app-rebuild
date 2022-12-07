f4cc7710a2899c1d2d8adf2ac4048dc5ac7ce611
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-12-07 10:10:24 +0000

    text shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,8 @@ module.exports = {
     colors: {
       background: "#000212",
       white: "#ffffff",
-      "white-a08" : "rgba(255, 255, 255, 0.08)"
+      "white-a08" : "rgba(255, 255, 255, 0.08)",
+      grey: "rgb(138, 143, 152)"
     },
     fontSize: {
       "xs": "1.3rem",
@@ -41,6 +42,9 @@ module.exports = {
     backgroundImage: {
       "primary-gradient":
         "linear-gradient(92.88deg, rgb(69, 94, 181) 9.16%, rgb(86, 67, 204) 43.89%, rgb(103, 63, 215) 64.72%)",
+    },
+    boxShadow: {
+      "primary": "rgb(80 63 205 / 50%) 0px 1px 40px",
     },
   },
   plugins: [],
