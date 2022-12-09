3ea83c39d91ed0a4cc4de0accf0cf8df37115d48
alekspanteli <alekspanteli@rambler.ru>
2022-12-09 11:10:39 +0000

    add animation

--- components/header.tsx
+++ components/header.tsx
@@ -17,21 +17,23 @@ export default function Header() {
         <Link href="/" className="flex items-center gap-2 text-md">
           <Logo className="h-[1.8rem] w-[1.8rem]" /> Linear
         </Link>
-        <div className={classNames("transtion-[visibility] md:visible", hamburgerMenuIsOpen ? "visible"
-                : "invisible delay-500" )}>
+        <div
+          className={classNames(
+            "transtion-[visibility] md:visible",
+            hamburgerMenuIsOpen ? "visible" : "invisible delay-500"
+          )}
+        >
           <nav
             className={classNames(
               "fixed top-navigation-height left-0 h-[calc(100vh_-_var(--navigation-height))] w-full overflow-auto bg-background transition-opacity duration-500 md:relative md:top-auto md:block md:h-auto md:w-auto md:bg-transparent md:opacity-100",
-              hamburgerMenuIsOpen
-              ? "opacity-100"
-              : "opacity-0")}
+              hamburgerMenuIsOpen ? "opacity-100" : "opacity-0"
+            )}
           >
             <ul
               className={classNames(
-                "ml-8 flex flex-col md:flex-row md:items-center",
-                "h-full md:gap-6 [&_a]:text-md [&_a]:transition-colors md:[&_a]:text-sm [&_a:hover]:text-grey",
-                "[&_a]:flex [&_a]:h-navigation-height [&_a]:w-full [&_a]:items-center [&_li]:border-b [&_li]:border-grey-dark [&_li]:md:border-none"
-              )}
+                "flex h-full flex-col md:flex-row md:items-center [&_li]:ml-8 [&_li]:border-b [&_li]:border-grey-dark md:[&_li]:border-none",
+                "ease-in [&_a]:flex [&_a]:h-navigation-height [&_a]:w-full [&_a]:translate-y-8 [&_a]:items-center [&_a]:text-lg [&_a]:transition-[color,transform] [&_a]:duration-300 md:[&_a]:translate-y-0 md:[&_a]:text-sm [&_a]:md:transition-colors [&_a:hover]:text-grey",
+                hamburgerMenuIsOpen && "[&_a]:translate-y-0")}
             >
               <li>
                 <Link href="#">Features</Link>
